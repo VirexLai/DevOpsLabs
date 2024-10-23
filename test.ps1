0dec53a3b9829e7d0fce780f99e2d29fd483d3e5
VirexLai <35486164+VirexLai@users.noreply.github.com>
2024-10-23 11:49:14 +0800

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -8,7 +8,7 @@ $VSubNetName = "TestVSubNet"
 $VSubNetRange = "11.0.0.0/24"
 
 ## 新增資源群組 ##
-New-AzResourceGroup -Name $RGName -Location $location
+New-AzResourceGroup -Name "TestRG" -Location "Japan East"
 
 ## 新增虛擬網路 ##
 New-AzVirtualNetwork -ResourceGroupName $RGName -Name $VNetName -Location $location -AddressPrefix $VNetRagne
