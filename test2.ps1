46964c2b6fb5d7514344e6d9a17d8cd7ae773af7
VirexLai <35486164+VirexLai@users.noreply.github.com>
2024-10-29 10:16:04 +0800

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -1,3 +1,5 @@
+## 從 YAML 內建立的 ResourceGroup 找出 VM 並儲存資訊 ## 
+$vm = Get-AzVM -ResourceGroupName $RGName 
 ## 指派 Role 給特定帳號 ##
 $AssignAccount = "Guest1@virex.online"
 # New-AzRoleAssignment -SignInName $AssignAccount -RoleDefinitionName "Contributor" -ResourceGroupName $RGName
