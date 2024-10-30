2a1f9f2586200b17f4a1ab341fa3de7d5e34e40d
VirexLai <35486164+VirexLai@users.noreply.github.com>
2024-10-30 10:05:10 +0800

    Update assignrole.ps1

--- assignrole.ps1
+++ assignrole.ps1
@@ -1,5 +1,5 @@
 ## 從 YAML 內建立的 ResourceGroup 找出 VM 並儲存資訊 ## 
-$vm = Get-AzVM -ResourceGroupName ${env:RGNAME} 
+$vm = Get-AzVM -ResourceGroupName ${env:RGName} 
 
 ## 指派 Role 給特定帳號 ##
 $AssignAccount = "Guest1@virex.online"
