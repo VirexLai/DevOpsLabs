d7e7016f0563d52a637cf45a5b6074c576637146
VirexLai <35486164+VirexLai@users.noreply.github.com>
2024-10-30 09:50:36 +0800

    Update AssignRole.ps1

--- AssignRole.ps1
+++ AssignRole.ps1
@@ -1,14 +1,25 @@
 ## 從 YAML 內建立的 ResourceGroup 找出 VM 並儲存資訊 ## 
 $vm = Get-AzVM -ResourceGroupName  ${env:RGNAME} 
 
+echo "vm info here"
+echo $vm
+
 ## 指派 Role 給特定帳號 ##
 $AssignAccount = "Guest1@virex.online"
 
+echo "Account info here"
+echo $AssignAccount
+
 ## 計算 vm 數量 ##
 $vmnum = $vm.id.count
+
+echo "vm numbers here"
+echo $vmnum
 
 ## 指派 Role ##
 for($i = 0; $i -le $vmnum ; $i++)
 {
     New-AzRoleAssignment -SignInName $AssignAccount -RoleDefinitionName "Contributor" -Scope $vm.id[$i] 
+    echo "i number is..."
+    echo $i
 }
